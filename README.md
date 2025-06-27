2d6f56b1ecadcda5156b8310af59d99cc0c81446
William Fernandes <wiilfern1910@outlook.com>
2025-06-27 15:22:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CloudCart
+A robust and scalable E-commerce REST API built with Spring Boot. Features secure authentication with JWT, Stripe integration for payments, and Cloudinary for image uploads.
---
+++ src/test/java/com/liamfer/CloudCart/CloudCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liamfer.CloudCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
