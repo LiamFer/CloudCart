5288930141df9e079b1cfd75903fa273e00feccc
William Fernandes <wiilfern1910@outlook.com>
2025-06-30 10:14:06 -0300

    feat: User Repository e User Details Service

---
+++ src/main/java/com/liamfer/CloudCart/service/UserDetailService.java
@@ -0,0 +1,18 @@
+package com.liamfer.CloudCart.service;
+
+import com.liamfer.CloudCart.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+    public UserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).get();
+    }
+}
