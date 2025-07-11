11590bc4ea75fc584b3cd895d1dcdc1d88ff729e
William Fernandes <wiilfern1910@outlook.com>
2025-07-11 09:09:14 -0300

    feat: Find Order Item by Product ID

--- src/main/java/com/liamfer/CloudCart/repository/OrderItemRepository.java
+++ src/main/java/com/liamfer/CloudCart/repository/OrderItemRepository.java
@@ -3,5 +3,9 @@ package com.liamfer.CloudCart.repository;
 import com.liamfer.CloudCart.entity.OrderItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderItemRepository extends JpaRepository<OrderItemEntity,Long> {
+    Optional<List<OrderItemEntity>> findAllByProductId(Long id);
 }
