d4f9ead6bbdab0c0bb9cc321813d246afebf6bf7
William Fernandes <wiilfern1910@outlook.com>
2025-07-13 10:38:09 -0300

    feat: Documentação Admin Post e Put

--- src/main/java/com/liamfer/CloudCart/controller/AdminController.java
+++ src/main/java/com/liamfer/CloudCart/controller/AdminController.java
@@ -1,9 +1,15 @@
 package com.liamfer.CloudCart.controller;
 
+import com.liamfer.CloudCart.dto.APIMessage;
 import com.liamfer.CloudCart.dto.image.ImageResponseDTO;
 import com.liamfer.CloudCart.dto.product.ProductDTO;
 import com.liamfer.CloudCart.dto.product.ProductResponseDTO;
 import com.liamfer.CloudCart.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -18,35 +24,50 @@ import java.util.List;
 @SecurityRequirement(name = "bearerAuth")
 public class AdminController {
     private final ProductService productService;
+
     public AdminController(ProductService productService) {
         this.productService = productService;
     }
 
+    @Operation(summary = "Adiciona um novo Produto no Site")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Produto adicionado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = APIMessage.class)))
+    })
     @PostMapping("/products")
-    public ResponseEntity<ProductResponseDTO> createNewProduct(@RequestBody @Valid ProductDTO newProduct){
+    public ResponseEntity<ProductResponseDTO> createNewProduct(@RequestBody @Valid ProductDTO newProduct) {
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.createNewProduct(newProduct));
     }
 
+    @Operation(summary = "Edita os dados de um Produto do Site")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Produto editado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = APIMessage.class))),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = APIMessage.class)))
+    })
     @PutMapping("/products/{id}")
     public ResponseEntity<ProductResponseDTO> updateProduct(@PathVariable("id") Long id,
-                                                            @RequestBody @Valid ProductDTO product){
-        return ResponseEntity.status(HttpStatus.OK).body(productService.updateProduct(id,product));
+                                                            @RequestBody @Valid ProductDTO product) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.updateProduct(id, product));
     }
 
     @DeleteMapping("/products/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id){
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id) {
         productService.deleteProduct(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @PostMapping("/products/{id}/images")
     public ResponseEntity<List<ImageResponseDTO>> uploadImages(@PathVariable("id") Long productID,
-                                                               @RequestPart("image") List<MultipartFile> images){
-        return ResponseEntity.status(HttpStatus.CREATED).body(productService.uploadProductImages(productID,images));
+                                                               @RequestPart("image") List<MultipartFile> images) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.uploadProductImages(productID, images));
     }
 
     @DeleteMapping("/products/images/{id}")
-    public ResponseEntity<List<ImageResponseDTO>> deleteImage(@PathVariable("id") Long imageID){
+    public ResponseEntity<List<ImageResponseDTO>> deleteImage(@PathVariable("id") Long imageID) {
         productService.deleteProductImage(imageID);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
