050af4eaf16bd105990af41fa3f6187420bc9a75
William Fernandes <wiilfern1910@outlook.com>
2025-07-15 12:54:39 -0300

    docs: Documentação Endpoint Products

--- src/main/java/com/liamfer/CloudCart/controller/ProductController.java
+++ src/main/java/com/liamfer/CloudCart/controller/ProductController.java
@@ -1,7 +1,13 @@
 package com.liamfer.CloudCart.controller;
 
+import com.liamfer.CloudCart.dto.APIMessage;
 import com.liamfer.CloudCart.dto.product.ProductSimpleDTO;
 import com.liamfer.CloudCart.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -16,11 +22,19 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Operation(summary = "Retorna informações dos Produtos")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Dados retornados com sucesso")})
     @GetMapping
     public ResponseEntity<Page<ProductSimpleDTO>> getProducts(Pageable pageable){
         return ResponseEntity.status(HttpStatus.OK).body(productService.findAllProducts(pageable));
     }
 
+    @Operation(summary = "Retorna informações de um Produto Específico")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Dados retornados com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = APIMessage.class)))})
     @GetMapping("/{id}")
     public ResponseEntity<ProductSimpleDTO> getProduct(@PathVariable("id") Long id){
         return ResponseEntity.status(HttpStatus.OK).body(productService.findProductById(id));
