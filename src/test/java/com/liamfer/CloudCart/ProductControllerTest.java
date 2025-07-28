0d30155596935df138edd79ee660a1d2ba1732fe
William Fernandes <wiilfern1910@outlook.com>
2025-07-28 19:48:08 -0300

    feat: Product Controller Test Building

---
+++ src/test/java/com/liamfer/CloudCart/ProductControllerTest.java
@@ -0,0 +1,26 @@
+package com.liamfer.CloudCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnProductById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/products/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Produto com ID: 1"));
+    }
+}
