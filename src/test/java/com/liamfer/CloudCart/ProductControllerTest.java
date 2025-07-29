c50d2b85a79dcd349c00c1fb5d0bf2619669acd3
William Fernandes <wiilfern1910@outlook.com>
2025-07-29 20:10:07 -0300

    feat: WebConfig e tentando Testar :(

--- src/test/java/com/liamfer/CloudCart/ProductControllerTest.java
+++ src/test/java/com/liamfer/CloudCart/ProductControllerTest.java
@@ -1,22 +1,17 @@
 package com.liamfer.CloudCart;
 
-import com.liamfer.CloudCart.controller.ProductController;
-import com.liamfer.CloudCart.repository.UserRepository;
-import com.liamfer.CloudCart.service.JWTService;
 import com.liamfer.CloudCart.service.ProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;  // IMPORT CORRETO
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,16 +23,11 @@ public class ProductControllerTest {
     @MockBean
     private ProductService productService;
 
-    @MockBean
-    private JWTService jwtService;
-
-    @MockBean
-    private UserRepository userRepository;
-
     @Test
     public void shouldReturnProductById() throws Exception {
-        mockMvc.perform(get("/products"))
+        mockMvc.perform(get("/products/34232")
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").isArray());
+                .andDo(print());
     }
 }
