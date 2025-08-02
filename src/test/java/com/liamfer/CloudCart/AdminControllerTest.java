1dfddd71b661c2460191b959c6abe3946de1f02e
William Fernandes <wiilfern1910@outlook.com>
2025-08-02 15:35:24 -0300

    feat: Função pra Mockar o Admin JWT Token

---
+++ src/test/java/com/liamfer/CloudCart/AdminControllerTest.java
@@ -0,0 +1,42 @@
+package com.liamfer.CloudCart;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.liamfer.CloudCart.dto.product.ProductDTO;
+import com.liamfer.CloudCart.dto.user.LoginUserDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AdminControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    String generateAuthToken() throws Exception {
+        LoginUserDTO dto = new LoginUserDTO("admin@email.com", "123456");
+        String loginPayload = new ObjectMapper().writeValueAsString(dto);
+        MvcResult result = mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginPayload))
+                .andExpect(status().isOk())
+                .andReturn();
+        String responseBody = result.getResponse().getContentAsString();
+        return JsonPath.read(responseBody, "$.token");
+    }
+
+}
