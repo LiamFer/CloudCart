4a07d5ca389ed6e6293cb835baef1bbdefee934b
William Fernandes <wiilfern1910@outlook.com>
2025-08-02 15:49:36 -0300

    test: PUT Update Product By Id

--- src/test/java/com/liamfer/CloudCart/AdminControllerTest.java
+++ src/test/java/com/liamfer/CloudCart/AdminControllerTest.java
@@ -1,5 +1,6 @@
 package com.liamfer.CloudCart;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import com.liamfer.CloudCart.dto.product.ProductDTO;
@@ -12,8 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,15 +40,34 @@ public class AdminControllerTest {
     }
 
     @Test
-    void shouldCreateNewProduct() throws Exception {
+    void shouldCreateUpdateAndDeleteProduct() throws Exception {
+        String token = this.generateAuthToken();
+        // Testando a Criação do Produto
         ProductDTO dto = new ProductDTO("Tênis Nike Air", "Confortável e estiloso", 499.90, 10, true);
         String json = new ObjectMapper().writeValueAsString(dto);
-        mockMvc.perform(post("/admin/products")
+        MvcResult postResult = mockMvc.perform(post("/admin/products")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .header("Authorization", "Bearer " + this.generateAuthToken())
+                        .header("Authorization", "Bearer " + token)
                         .content(json))
                 .andExpect(status().isCreated())
-                .andDo(print());
+                .andReturn();
+        String responseJson = postResult.getResponse().getContentAsString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(responseJson);
+        Long productId = jsonNode.get("id").asLong();
+        System.out.printf("CREATED: Produto %d criado com Sucesso!\n",productId);
+
+        // Testando Update no Produto
+        ProductDTO putDto = new ProductDTO("Tênis Adidas Speed 2", "Meia Maratona Ideal", 199.90, 15, true);
+        String putJson = new ObjectMapper().writeValueAsString(putDto);
+        MvcResult putResult = mockMvc.perform(put("/admin/products/"+productId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + token)
+                        .content(putJson))
+                .andExpect(status().isOk())
+                .andReturn();
+        System.out.printf("PUT: Produto %d editado com Sucesso!\n",productId);
+
     }
 
 }
